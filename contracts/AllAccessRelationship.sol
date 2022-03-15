34f0a9ee7926cb4bbf26e5664f03a1033048659d
Mustafa Tariq <mustafa@tariqk.tech>
2022-03-15 15:08:03 -0400

    all access created

--- contracts/AllAccessRelationship.sol
+++ contracts/AllAccessRelationship.sol
@@ -1,56 +1,75 @@
 pragma solidity ^0.4.18;
 
 contract AllAccessRelationship {
-    address public patron;
-    address public provider;
-    string public providerName;
+  address public patron;
+  address public provider;
+  string public providerName;
 
-    struct Viewer {
-        string name;
-        address addr;
+  struct Viewer {
+    string name;
+    address addr;
+  }
+
+  address[] public viewers;
+  mapping(address => bool) public isViewer;
+  mapping(address => Viewer) viewerInfo;
+  mapping(string => address) viewerByName;
+
+  uint256 constant UINT256_MAX = ~uint256(0);
+
+  modifier isPatron() {
+    if(msg.sender != patron) revert();
+    _;
+  }
+
+  function Relationship(address _provider) public {
+    patron = msg.sender;
+    provider = _provider;
+  }
+
+  function setProviderName(string name) public {
+    providerName = name;
+  }
+
+  function addViewer(string name, address viewer) public isPatron {
+    require(!isViewer[viewer]);
+
+    isViewer[viewer] = true;
+    viewers.push(viewer);
+    viewerInfo[viewer] = Viewer(name, viewer);
+  }
+
+  function removeViewer(address viewer) public isPatron {
+    require(isViewer[viewer]);
+
+    isViewer[viewer] = false;
+    bool overwrite = false;
+    for(uint i = 0; i < viewers.length; i++) {
+      if(overwrite) {
+        viewers[i - 1] = viewers[i];
+      }
+      if(viewers[i] == viewer) {
+        overwrite = true;
+      }
     }
+    delete(viewers[viewers.length-1]);
+    delete(viewerInfo[viewer]);
+  }
 
-    address[] public viewers;
-    mapping(address => bool) public isViewer;
-    mapping(address => Viewer) viewerInfo;
-    mapping(string => address) viewerByName;
+  function getNumViewers() public constant returns(uint) {
+    return viewers.length;
+  }
 
-    uint256 constant UINT256_MAX = ~uint256(0);
+  function getViewerByName(string name) public constant returns(address) {
+    return viewerByName[name];
+  }
 
-    modifier isPatron() {
-        if(msg.sender != patron) revert();
-        _;
-    }
+  function getViewerName(address addr) public constant returns(string) {
+    return viewerInfo[addr].name;
+  }
 
-    function Relationship(address _provider) public {
-        
-    }
-
-    function setProviderName(string name) public {
-        
-    }
-
-    function addViewer(string name, address viewer) public isPatron {
-        
-    }
-
-    function removeViewer(address viewer) public isPatron {
-        
-    }
-
-    function getNumViewers() public constant returns(uint) {
-        
-    }
-
-    function getViewerByName(string name) public constant returns(address) {
-        
-    }
-
-    function getViewerName(address addr) public constant returns(string) {
-        
-    }
-
-    function terminate() public {
-        
-    }
+  function terminate() public {
+      if(msg.sender != patron && msg.sender != provider) revert();
+      selfdestruct(patron);
+  }
 }
